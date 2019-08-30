6b261be5288b4e6ed2b1f851e15ae1dd1633340c
Rebecca H <rhernandez@skyslope.com>
2019-08-30 15:11:09 -0700

    functions added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,6 +18,22 @@ var connection = mysql.createConnection({
 connection.connect(function (err, res) {
     if (err) throw err;
     console.log(res);
-    // console.log("connected as id " + connection.threadId + "\n");
-    // readData();
+    console.log("connected as id " + connection.threadId + "\n");
+    readData();
 });
+
+function readData() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].id + "|" + res[i].product + "|" + res[i].department + "|" + res[i].price + "|" + res[i].genre);
+        }
+        console.log("-----------------------------------");
+
+
+        // if (err) throw err;
+
+        // // Log all results of the SELECT statement
+        // console.log(res);
+        // connection.end();
+    });
+}
